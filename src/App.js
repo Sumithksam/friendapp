4162fbf8f3416a75e0cd0eddad0b85de463446c7
Sumithksam <sumith77sam@gmail.com>
2022-05-19 22:20:23 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Addfriends from './components/Addfriends';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Viewfriend from './components/Viewfriend';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+  <Routes>
+<Route path='/' exact element={<Addfriends/>}/>
+<Route path="/view" exact element={<Viewfriend/>}/>
+
+
+  </Routes>
+  </BrowserRouter>
   );
 }
 
